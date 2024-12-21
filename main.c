1de42bf3308d8ef2019545731309af0708323e34
Masahiro Yamashita <milky.tafl.0225@gmail.com>
2024-12-21 09:33:41 +0000

    C + 計算

--- main.c
+++ main.c
@@ -1,7 +1,26 @@
+// #include <stdio.h>
+
+// int main(void)
+// {
+//   printf("hello, world!\n");
+//   return 0;
+// }
+
+// #include <stdio.h>
+
+// int main(void)
+// {
+//   printf("山下将大\n");
+//   printf("29歳\n");
+//   printf("彼女の顎は折れている\n");
+//   return 0;
+// }
+
 #include <stdio.h>
 
 int main(void)
 {
-  printf("hello, world!\n");
+  printf("29 + 29の計算をします\n");
+  printf("%d\n", 29 + 29);
   return 0;
 }
