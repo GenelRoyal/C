4558242f317f10c4e2feb6892d03eb4904508483
Masahiro Yamashita <milky.tafl.0225@gmail.com>
2024-12-23 07:25:49 +0000

    データ型

--- practice2.c
+++ practice2.c
@@ -1,7 +1,19 @@
+// #include <stdio.h>
+
+// int main(void)
+// {
+//   printf("%dバイト\n", (int)(sizeof(char)));
+//   return 0;
+// }
+
+//int population = 121'270'000 //入れる位置は自由(実行時は無視される)  
+
 #include <stdio.h>
 
 int main(void)
 {
-  printf("%dバイト\n", (int)(sizeof(char)));
+  char c = 100;
+  char d = c + 100;  //ここで溢れるはず！
+  printf("%d\n", d);
   return 0;
 }
