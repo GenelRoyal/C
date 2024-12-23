c565b43d5ad860aad2f8d2ac188ad2cd3fd38d8c
Masahiro Yamashita <milky.tafl.0225@gmail.com>
2024-12-23 04:26:36 +0000

    インデント＆コメント

---
+++ practice.c
@@ -0,0 +1,19 @@
+// #include <stdio.h>
+
+// int main(void)
+// {
+//   printf("日記ソフトを開始します\n");
+//   printf("終了します\n");
+//   return 0;
+// }
+
+#include <stdio.h>
+
+// mainブロック
+int main(void)
+{
+  int age; //年齢を入れる箱
+  age = 20;
+  printf("私は%d歳です\n", age);
+  return 0;
+}
--- sample.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-  printf("日記ソフトを開始します\n");
-  printf("終了します\n");
-  return 0;
-}
