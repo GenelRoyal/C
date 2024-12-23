4cdaff7724f8430726e2fadec2af2998c6488ca4
Masahiro Yamashita <milky.tafl.0225@gmail.com>
2024-12-23 05:13:10 +0000

    1章完

--- practice.c
+++ practice.c
@@ -7,13 +7,22 @@
 //   return 0;
 // }
 
+// #include <stdio.h>
+
+// // mainブロック
+// int main(void)
+// {
+//   int age; //年齢を入れる箱
+//   age = 20;
+//   printf("私は%d歳です\n", age);
+//   return 0;
+// }
+
 #include <stdio.h>
 
-// mainブロック
 int main(void)
 {
-  int age; //年齢を入れる箱
-  age = 20;
-  printf("私は%d歳です\n", age);
+  printf("プログラムを開始します\n");
+  printf("プログラムを終了します\n");
   return 0;
 }
