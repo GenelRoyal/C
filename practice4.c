105ebd8a8b2e967603281f06131a295667b7011a
Masahiro Yamashita <milky.tafl.0225@gmail.com>
2024-12-25 07:38:32 +0000

    練習問題

--- practice4.c
+++ practice4.c
@@ -39,6 +39,43 @@
 // }
 
 
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// typedef char String[1024];
+
+// int main(void)
+// {
+//   String answerNo;
+//   printf("かっこよくて最高な、C言語男子の名前は？\n");
+//   printf("1:ミナト 2:genel 3:ツバサ 4:タカシ\n");
+//   scanf("%s", answerNo);
+//   int n = atoi(answerNo);  //入力された数値の文字列をintに変換
+//   if (n == 2)　　　　　　 //数値なら==で比較してok!
+//     printf("大正解！ 見る目あるね！\n");
+//   else
+//     printf("残念。\n");
+//   return 0;
+// }
+
+
+#include <stdio.h>
+
+int main(void)
+{
+  printf("いただきます\nバナナを食べます\n");
+  
+  bool more = false;
+  if (more)
+    printf("おかわりをください\n");
+  else
+    printf("お腹いっぱいです\n");
+  printf("ごちそうさまでした\n");
+  return 0;
+}
+
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -46,14 +83,20 @@ typedef char String[1024];
 
 int main(void)
 {
-  String answerNo;
-  printf("かっこよくて最高な、C言語男子の名前は？\n");
-  printf("1:ミナト 2:genel 3:ツバサ 4:タカシ\n");
-  scanf("%s", answerNo);
-  int n = atoi(answerNo);  //入力された数値の文字列をintに変換
-  if (n == 2)　　　　　　 //数値なら==で比較してok!
-    printf("大正解！ 見る目あるね！\n");
-  else
-    printf("残念。\n");
+  int temp = 30;
+  int ansNo =1;
+  String answer;
+  
+  while (ansNo == 1) {
+    printf("現在の設定温度：%d\n", temp);
+    printf("暑いですか？ Yes=1 No=2\n");
+    scanf("%s", answer);
+    ansNo = atoi(answer);
+    if (ansNo == 1) {
+      temp = temp - 1;
+    } else {
+      printf("設定を終了します\n");
+    }
+  }
   return 0;
 }
