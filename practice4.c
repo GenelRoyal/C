eb78f8810aee98cc608358434e8527c56c418763
Masahiro Yamashita <milky.tafl.0225@gmail.com>
2024-12-25 07:35:00 +0000

    文字列の比較

--- practice4.c
+++ practice4.c
@@ -25,15 +25,35 @@
 // }
 
 
+// #include <stdio.h>
+
+// int main(void)
+// {
+//   bool tenki = false;
+//   if (tenki == true) { //内容が2行なので波括弧省略不可
+//     printf("洗濯をする\n");
+//     printf("散歩をする\n");
+//   } else //内容が1行なので波括弧省略可能
+//     printf("映画を観る\n");
+//   return 0;
+// }
+
+
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef char String[1024];
 
 int main(void)
 {
-  bool tenki = false;
-  if (tenki == true) { //内容が2行なので波括弧省略不可
-    printf("洗濯をする\n");
-    printf("散歩をする\n");
-  } else //内容が1行なので波括弧省略可能
-    printf("映画を観る\n");
+  String answerNo;
+  printf("かっこよくて最高な、C言語男子の名前は？\n");
+  printf("1:ミナト 2:genel 3:ツバサ 4:タカシ\n");
+  scanf("%s", answerNo);
+  int n = atoi(answerNo);  //入力された数値の文字列をintに変換
+  if (n == 2)　　　　　　 //数値なら==で比較してok!
+    printf("大正解！ 見る目あるね！\n");
+  else
+    printf("残念。\n");
   return 0;
 }
