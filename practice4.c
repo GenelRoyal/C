83fe22fd8179c3386fc103a0e5623c6211c9331d
Masahiro Yamashita <milky.tafl.0225@gmail.com>
2024-12-25 07:31:17 +0000

    while文

--- practice4.c
+++ practice4.c
@@ -1,13 +1,25 @@
+// #include <stdio.h>
+
+// int main(void)
+// {
+//   bool tenki = true;
+//   if (tenki == true) { //もし変数tenkiがtrueだったら
+//     printf("洗濯をします\n");
+//     printf("散歩に行きます\n");
+//   } else { //そうでなければ
+//     printf("映画を観ます\n");
+//   }
+//   return 0;
+// }
+
+
 #include <stdio.h>
 
 int main(void)
 {
-  bool tenki = true;
-  if (tenki == true) { //もし変数tenkiがtrueだったら
-    printf("洗濯をします\n");
-    printf("散歩に行きます\n");
-  } else { //そうでなければ
-    printf("映画を観ます\n");
+  bool lowCharging = true;
+  while (lowCharging == true) { //バッテリーが少ない間は
+    printf("30分充電する\n");
   }
   return 0;
 }
