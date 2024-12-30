d88881cd8f8ccbfd1f5cc4dc039f81c9b5add884
Masahiro Yamashita <milky.tafl.0225@gmail.com>
2024-12-30 13:32:29 +0000

    パターン２ ループによる集計

--- practice7.c
+++ practice7.c
@@ -66,3 +66,23 @@ int main(void)
   
   return 0;
 }
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+  int scores[] = {75, 57, 90, 46, 82}; //５教科のテスト結果
+  int sum = 0; //合計を0でリセット
+  for (int i = 0; i < 5; i++) {
+    sum += scores[i]; //１科目ずつsumに足していく
+  }
+  int avg = sum / 5; //合計を科目数で割る
+  printf("合計点：%d\n", sum);
+  printf("平均点：%d\n", avg);
+  return 0;
+}
+
+
