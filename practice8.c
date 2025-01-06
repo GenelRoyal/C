f0b21f78ac274ef78b67360b65688c39e59f785c
Masahiro Yamashita <milky.tafl.0225@gmail.com>
2025-01-06 10:49:00 +0000

    関数の呼び出し

---
+++ practice8.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void hello(void) //hello関数の本体
+{
+  printf("こんにちは\n");
+}
+
+int main(void) //main関数
+{
+  printf("関数を呼び出します\n");
+  hello(); //hello関数を呼び出す
+  printf("関数の呼び出しが終わりました\n");
+  
+  return 0;
+}
