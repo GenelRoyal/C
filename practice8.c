2b041c829669f8d3ff70fd0ca230308ea3b6d383
Masahiro Yamashita <milky.tafl.0225@gmail.com>
2025-01-06 10:50:40 +0000

    main関数以外からの関数呼び出し

--- practice8.c
+++ practice8.c
@@ -1,15 +1,37 @@
+// #include <stdio.h>
+
+// void hello(void) //hello関数の本体
+// {
+//   printf("こんにちは\n");
+// }
+
+// int main(void) //main関数
+// {
+//   printf("関数を呼び出します\n");
+//   hello(); //hello関数を呼び出す
+//   printf("関数の呼び出しが終わりました\n");
+  
+//   return 0;
+// }
+
+
+
 #include <stdio.h>
 
-void hello(void) //hello関数の本体
+
+void funcB(void)
 {
-  printf("こんにちは\n");
+  printf("関数Bです\n");
+}
+
+void funcA(void)
+{
+  printf("関数Aです\n");
+  funcB();
 }
 
 int main(void) //main関数
 {
-  printf("関数を呼び出します\n");
-  hello(); //hello関数を呼び出す
-  printf("関数の呼び出しが終わりました\n");
-  
+  funcA();
   return 0;
 }
