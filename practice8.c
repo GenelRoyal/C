f516666297df26e3864e0246b8650c532c9354c2
Masahiro Yamashita <milky.tafl.0225@gmail.com>
2025-01-07 13:01:19 +0000

    引数の利用

--- practice8.c
+++ practice8.c
@@ -38,24 +38,49 @@
 
 
 
+// #include <stdio.h>
+
+// void funcA(void); //funcAの存在を表明
+// void funcB(void); //funcBの存在を表明
+
+// int main(void)
+// {
+//   funcA();
+//   return 0;
+// }
+
+// void funcA(void) 
+// {
+//   printf("関数Aです\n");
+//   funcB();
+// }
+
+// void funcB(void)
+// {
+//   printf("関数Bです\n");
+// }
+
+
 #include <stdio.h>
 
-void funcA(void); //funcAの存在を表明
-void funcB(void); //funcBの存在を表明
+void hello(int no)
+{
+  if (no == 1) {
+    printf("岬さん、こんにちは\n");
+  } else if (no == 2) {
+    printf("赤城さん、こんにちは\n");
+  } else {
+    printf("海藤さん、こんにちは\n");
+  }
+}
 
 int main(void)
 {
-  funcA();
+  printf("関数を呼び出します\n");
+  hello(1); //引数1を渡しながら関数を呼び出す
+  hello(2); //引数2を渡しながら関数を呼び出す
+  hello(3); //引数3を渡しながら関数を呼び出す
+  printf("関数の呼び出しが終わりました\n");
+  
   return 0;
-}
-
-void funcA(void) 
-{
-  printf("関数Aです\n");
-  funcB();
-}
-
-void funcB(void)
-{
-  printf("関数Bです\n");
 }
