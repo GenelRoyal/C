c026a2cd9903e46729d08d6fd22cd305c370cc3b
Masahiro Yamashita <milky.tafl.0225@gmail.com>
2025-01-14 09:40:50 +0000

    第2部 8章完

--- practice8.c
+++ practice8.c
@@ -106,24 +106,99 @@
 
 
 
+// #include <stdio.h>
+
+// int add(int x, int y)
+// {
+//   int ans = x + y;
+//   return ans;
+// }
+
+// int main(void)
+// {
+//   int year = 2025;
+  
+//   int ans1;
+//   ans1 = add(year, 4); //addの結果が変数ans1に代入される
+//   printf("%d年の%d年後は%d年です\n", year, 4 ,ans1);
+  
+//   int ans2 = add(year, 50); //変数宣言と同時に呼び出しても良い
+//   printf("%d年は%d年の%d年後です\n", ans2, year, 50);
+  
+//   return 0;
+// }
+
+
+
+
 #include <stdio.h>
 
-int add(int x, int y)
+bool isLeapYear(int year)
 {
-  int ans = x + y;
-  return ans;
+  return (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0));
 }
 
 int main(void)
 {
-  int year = 2025;
+  int year = 2000;
   
-  int ans1;
-  ans1 = add(year, 4); //addの結果が変数ans1に代入される
-  printf("%d年の%d年後は%d年です\n", year, 4 ,ans1);
-  
-  int ans2 = add(year, 50); //変数宣言と同時に呼び出しても良い
-  printf("%d年は%d年の%d年後です\n", ans2, year, 50);
-  
+  if (isLeapYear(year)) {
+    printf("%d年は、閏年です。\n", year);
+  } else {
+    printf("%d年は、閏年ではありません。\n", year);
+  }
+  return 0;
+}
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef char String[1024];
+
+int iscanf(void)
+{
+  String inputStr;
+  scanf("%s", inputStr);
+  return atoi(inputStr);
+}
+
+int calcPayment(int dividend, int divisor)
+{
+  // 総額を人数で割る(端数も保持)
+  double dnum = (double)dividend / divisor;
+  // 100円未満を切り捨ててみる
+  int person = (int)(dnum / 100) * 100;
+  // 元の値と比較して、小さければ100円未満があったので上乗せ
+  if (dnum > person) {
+    person = person + 100;
+  }
+  return person;
+}
+
+void showPayment(int general, int manager, int numbers)
+{
+  printf("*** 支払額 ***¥n");
+  printf("1人あたり%d円（%d人）、幹事は%d円です。¥n", general, numbers - 1, manager);
+}
+
+int main(void)
+{
+  // 計算データの入力
+  printf("支払総額を入力してください：");
+  int amount = iscanf();
+  printf("参加人数を入力してください：");
+  int people = iscanf();
+
+  // 割り勘の計算
+  int pay = calcPayment(amount, people);
+
+  // 幹事の支払額を計算
+  int payorg = amount - pay * (people - 1);
+
+  // 結果の表示
+  showPayment(pay, payorg, people);
+
   return 0;
 }
